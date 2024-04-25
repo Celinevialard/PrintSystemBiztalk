bd0aba164a0fd68ca05857adec3945410d491445
Celine <celine.vialard@gmail.com>
2024-04-25 17:40:23 +0200

    add correlation

--- PrintSystemBiztalk/PrintSystemBiztalk/Schemas/Auth.xsd.cs
+++ PrintSystemBiztalk/PrintSystemBiztalk/Schemas/Auth.xsd.cs
@@ -7,8 +7,11 @@ namespace PrintSystemBiztalk.Schemas {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [SchemaType(SchemaTypeEnum.Document)]
     [Schema(@"http://PrintSystemBiztalk.Schemas.Auth",@"Auth")]
+    [Microsoft.XLANGs.BaseTypes.PropertyAttribute(typeof(global::PrintSystemBiztalk.PropertySchema.RequestIdentification), XPath = @"/*[local-name()='Auth' and namespace-uri()='http://PrintSystemBiztalk.Schemas.Auth']/*[local-name()='RequestIdentification' and namespace-uri()='']", XsdType = @"string")]
+    [Microsoft.XLANGs.BaseTypes.DistinguishedFieldAttribute(typeof(System.String), "RequestIdentification", XPath = @"/*[local-name()='Auth' and namespace-uri()='http://PrintSystemBiztalk.Schemas.Auth']/*[local-name()='RequestIdentification' and namespace-uri()='']", XsdType = @"string")]
     [System.SerializableAttribute()]
     [SchemaRoots(new string[] {@"Auth"})]
+    [Microsoft.XLANGs.BaseTypes.SchemaReference(@"PrintSystemBiztalk.PropertySchema.PropertySchema", typeof(global::PrintSystemBiztalk.PropertySchema.PropertySchema))]
     public sealed class Auth : Microsoft.XLANGs.BaseTypes.SchemaBase {
         
         [System.NonSerializedAttribute()]
@@ -16,8 +19,23 @@ namespace PrintSystemBiztalk.Schemas {
         
         [System.NonSerializedAttribute()]
         private const string _strSchema = @"<?xml version=""1.0"" encoding=""utf-16""?>
-<xs:schema xmlns=""http://PrintSystemBiztalk.Schemas.Auth"" xmlns:b=""http://schemas.microsoft.com/BizTalk/2003"" targetNamespace=""http://PrintSystemBiztalk.Schemas.Auth"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+<xs:schema xmlns=""http://PrintSystemBiztalk.Schemas.Auth"" xmlns:b=""http://schemas.microsoft.com/BizTalk/2003"" xmlns:ns0=""https://PrintSystemBiztalk.PropertySchema"" targetNamespace=""http://PrintSystemBiztalk.Schemas.Auth"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+  <xs:annotation>
+    <xs:appinfo>
+      <b:imports>
+        <b:namespace prefix=""ns0"" uri=""https://PrintSystemBiztalk.PropertySchema"" location=""PrintSystemBiztalk.PropertySchema.PropertySchema"" />
+      </b:imports>
+    </xs:appinfo>
+  </xs:annotation>
   <xs:element name=""Auth"">
+    <xs:annotation>
+      <xs:appinfo>
+        <b:properties>
+          <b:property name=""ns0:RequestIdentification"" xpath=""/*[local-name()='Auth' and namespace-uri()='http://PrintSystemBiztalk.Schemas.Auth']/*[local-name()='RequestIdentification' and namespace-uri()='']"" />
+          <b:property distinguished=""true"" xpath=""/*[local-name()='Auth' and namespace-uri()='http://PrintSystemBiztalk.Schemas.Auth']/*[local-name()='RequestIdentification' and namespace-uri()='']"" />
+        </b:properties>
+      </xs:appinfo>
+    </xs:annotation>
     <xs:complexType>
       <xs:sequence>
         <xs:element name=""Username"">
@@ -34,6 +52,7 @@ namespace PrintSystemBiztalk.Schemas {
             </xs:simpleContent>
           </xs:complexType>
         </xs:element>
+        <xs:element name=""RequestIdentification"" type=""xs:string"" />
       </xs:sequence>
     </xs:complexType>
   </xs:element>
